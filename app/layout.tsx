4b7dddb65618b0581980c1970a5f55f65a238e2c
shakky24 <shakthiseshadri@gmail.com>
2025-04-12 14:58:57 +0530

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
             src="https://www.googletagmanager.com/ns.html?id=GTM-KZRGM7J7"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
         <ThemeProvider
